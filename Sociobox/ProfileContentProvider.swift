66b03af5e857c07737d4ecd11bf943c588c442bb
Icung <saiful-islam@outlook.co.id>
2023-07-02 11:31:18 +0700

    Using Dependency Injection

--- Sociobox/ProfileContentProvider.swift
+++ Sociobox/ProfileContentProvider.swift
@@ -1,15 +1,15 @@
-/// Copyright (c) 2020 Razeware LLC
-///
+/// Copyright (c) 2023 Razeware LLC
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// This project and source code may use libraries or frameworks that are
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
@@ -30,25 +30,49 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
-import UIKit
 import SwiftUI
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-  var window: UIWindow?
+protocol ProfileContentProviderProtocol {
+  var privacyLevel: PrivacyLevel { get }
+  var canSendMessage: Bool { get }
+  var canStartVideoChat: Bool { get }
+  var photosView: AnyView { get }
+  var feedView: AnyView { get }
+  var friendsView: AnyView { get }
+}
 
-  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-    
-    let provider = ProfileContentProviderView(privacyLevel: .friend, user: Mock.user())
-    let profileView = ProfileView(
-      provider: provider,
-      user: Mock.user()
-    )
-    
-    if let windowScene = scene as? UIWindowScene {
-      let window = UIWindow(windowScene: windowScene)
-      window.rootViewController = UIHostingController(rootView: profileView)
-      self.window = window
-      window.makeKeyAndVisible()
-    }
+final class ProfileContentProviderView: ProfileContentProviderProtocol {
+  let privacyLevel: PrivacyLevel
+  private let user: User
+  
+  init(privacyLevel: PrivacyLevel, user: User) {
+    self.privacyLevel = privacyLevel
+    self.user = user
+  }
+  
+  var canSendMessage: Bool {
+    privacyLevel > .everyone
+  }
+  
+  var canStartVideoChat: Bool {
+    privacyLevel > .everyone
+  }
+  
+  var photosView: AnyView {
+    privacyLevel > .everyone ?
+      AnyView(PhotosView(photos: user.photos)) :
+      AnyView(EmptyView())
+  }
+  
+  var feedView: AnyView {
+    privacyLevel > .everyone ?
+      AnyView(HistoryFeedView(posts: user.historyFeed)) :
+      AnyView(RestrictedAccessView())
+  }
+  
+  var friendsView: AnyView {
+    privacyLevel > .everyone ?
+      AnyView(UsersView(title: "Friends", users: user.friends)) :
+      AnyView(EmptyView())
   }
 }
